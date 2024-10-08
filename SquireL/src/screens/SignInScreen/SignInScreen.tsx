48df80c0af4bf872f3d8d9a3acff6cbdee584e0e
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-10-08 22:25:33 +0200

    feat/42 creating form sign in

--- SquireL/src/screens/SignInScreen/SignInScreen.tsx
+++ SquireL/src/screens/SignInScreen/SignInScreen.tsx
@@ -1,5 +1,10 @@
-import { ImageBackground, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { ImageBackground, StyleSheet, View} from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import * as SplashScreen from 'expo-splash-screen';
+import { MedievalSharp_400Regular } from '@expo-google-fonts/medievalsharp';
+import { XStack, YStack, Text, Button, Form, Label, Input } from 'tamagui';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
 
 type SignInScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -12,27 +17,107 @@ type Props = {
 
 export default function SignInScreen({ navigation }: Props) {
 
+  const [loaded, error] = useFonts({
+    MedievalSharp_400Regular,
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    } else if (error) {
+      console.log('Error loading fonts:', error);
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (    
       <ImageBackground style={styles.pageContainer} source={require('../../assets/images/welcomeScreen.jpg')}>
-        <View style={styles.container}>
-            <Text style={styles.text}>Sign In</Text>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('AppDrawer')}
+        <XStack flex={1}>
+          <YStack
+            flex={2}
+            gap="$3"
+            justifyContent="center"
+            alignItems="center"
+            backgroundColor="rgba(177, 176, 176, 0.27)"
+            padding
+            marginLeft='5%'
             >
-              <Text>Validate</Text>
-            </TouchableOpacity>
-            <TouchableOpacity 
-              onPress={() => navigation.navigate('SignUp')}
-            >
-              <Text>Sign Up</Text>
-            </TouchableOpacity>
-        </View>
+              <Form
+              gap="$3"
+              >
+                <Text
+                  fontSize={40}
+                  fontFamily="MedievalSharp_400Regular"
+                  color="#fff"
+                  >Welcome Back</Text>
+                <XStack
+                >
+                  <Label
+                  color="#fff"                
+                  fontFamily="MedievalSharp_400Regular"
+                  > 
+                    Username
+                  </Label>
+                  <Input
+                  flex={1}
+                  ></Input>
+                </XStack>  
+                <XStack>
+                  <Label
+                  color="#fff"
+                  fontFamily="MedievalSharp_400Regular"
+                  > 
+                    Password
+                  </Label>
+                  <Input
+                  flex={1}
+                  ></Input>
+                </XStack>  
+                
+                <Button
+                  onPress={() => navigation.navigate('AppDrawer')}>
+                  <Text 
+                    fontFamily="MedievalSharp_400Regular" 
+                    fontSize={20}
+                  >Sign In</Text>
+                </Button>
+                <Button 
+                  onPress={() => navigation.navigate('SignUp')}
+                >
+                  <Text 
+                    fontFamily="MedievalSharp_400Regular"
+                    fontSize={20}
+                    >Don't have an account? Sign up</Text>
+                </Button>
+              </Form>              
+              <Button
+                width="50%"
+                size="$2"
+                chromeless
+              >
+                <Text
+                   fontFamily="MedievalSharp_400Regular"
+                   fontSize={15}
+                   color="#fff"                   
+                >
+                  Forgot password?
+                </Text>
+              </Button>
+          </YStack>
+          <YStack 
+            flex={1}>            
+          </YStack>
+        </XStack> 
       </ImageBackground>    
   );
 }
 
 const styles = StyleSheet.create({
-    pageContainer: {
+  pageContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -45,7 +130,4 @@ const styles = StyleSheet.create({
     flex: 0.5,
     alignItems: 'center',
   },
-  text: {
-    color: '#fff' 
-  }
 });
