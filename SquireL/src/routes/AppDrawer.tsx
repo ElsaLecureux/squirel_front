8f0351b4a83ee456f91a23ffb4adcec29ea080c2
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-11-29 12:28:10 +0100

    feat/75 added sign out logic adn fixed some buttons icons on web

--- SquireL/src/routes/AppDrawer.tsx
+++ SquireL/src/routes/AppDrawer.tsx
@@ -1,5 +1,6 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { DrawerNavigationProp  } from '@react-navigation/drawer';
+import { useNavigation } from '@react-navigation/native';
 
 import HomeStack from './HomeStack';
 import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
@@ -10,8 +11,12 @@ import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons/faHouse';
 import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons/faArrowRightFromBracket';
+import { Platform } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 
 import { Button, XStack, YStack } from 'tamagui';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 const Drawer = createDrawerNavigator<AppDrawerParamList>();
 
@@ -20,10 +25,36 @@ type AppDrawerNavigationProp = DrawerNavigationProp <
 >;
 
 type Props = {
-  navigation: AppDrawerNavigationProp;
+  navigation: AppDrawerNavigationProp & StackNavigationProp<RootStackParamList>;
 };
 
 export default function AppDrawer ({navigation}: Props) {
+  const navigate = useNavigation();
+
+  const signOut = async () => {
+    if (Platform.OS === 'web'){
+      localStorage.removeItem('access_token');
+      if (!localStorage.getItem('access_token')){
+          //message grace au error message pour dire au revoir
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'Welcome' }],
+          })
+      }
+      //message pour prevenir que log out failed
+    }
+    try {
+      await SecureStore.deleteItemAsync('access_token'); 
+      //message grace au error message pour dire au revoir   
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'Welcome' }],
+      })
+    } catch (error) {
+      // utiliser message error
+     console.log('Something went wrong and log out failed');
+    };
+  }
 
   const renderHeaderRight = (currentRoute: string | undefined) => {
     if (currentRoute !== 'Home') {
@@ -31,7 +62,7 @@ export default function AppDrawer ({navigation}: Props) {
         <XStack
           marginRight='3%'
           marginTop='3%'
-          gap={10}
+          gap={12}
         >
           <Button
             size="$2"
@@ -56,7 +87,16 @@ export default function AppDrawer ({navigation}: Props) {
         <XStack
           marginRight='3%'
           marginTop='3%'
+          gap={12}
         >
+          <Button
+          size="$2"
+          height={40}
+          variant="outlined"
+          borderColor="#ff8a01" 
+          onPress={() => signOut()}>
+            <FontAwesomeIcon icon={faArrowRightFromBracket} style={{color: "#ff8a01",}} size={25}/>  
+          </Button>
           <Button
           size="$2"
           height={40}
