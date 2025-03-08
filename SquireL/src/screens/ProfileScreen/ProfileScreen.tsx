4dc572c43920341669a7a3eea89d5d63628768dd
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-03-08 18:44:34 +0100

    feat/121 created request in profil to geUserWonGames

--- SquireL/src/screens/ProfileScreen/ProfileScreen.tsx
+++ SquireL/src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,10 +1,12 @@
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Platform, ImageBackground, StyleSheet } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { Text, YStack, XStack } from 'tamagui';
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faPersonDigging } from '@fortawesome/free-solid-svg-icons/faPersonDigging';
 import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 type ProfileScreenNavigationProp = StackNavigationProp<
   AppDrawerParamList,
@@ -16,6 +18,35 @@ type Props = {
 };
 
 export default function ProfileScreen({ navigation }: Props) {
+
+  const [host, setHost] = useState('');
+  const API_URL = `http://${host}:3000/userPlayGame`;
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (Platform.OS === 'ios' || Platform.OS === 'android') {
+      setHost('10.117.60.67');
+    } else if (Platform.OS === 'web') {
+      setHost('localhost');
+    }
+    setIsReady(true);
+  }, []);
+
+  useEffect(() => {
+    if (!isReady) return; 
+    //add get infos user
+    const getUserWonGames = async () =>  {
+      const userId = 4;
+      const infos = await axios({
+        method: 'get',
+        url: `${API_URL}/${userId}`,
+      }).then(
+        
+      )
+      console.log(infos)
+    }
+    getGamesWonUser();
+   }, [isReady, host]);
 
   return (    
       <ImageBackground style={styles.pageContainer} source={require('../../assets/images/profileScreen.jpg')}>
