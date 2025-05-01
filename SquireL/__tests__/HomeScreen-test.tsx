425a7bc30c8a5975f5616eb6025e967769a91fbd
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-05-01 12:42:20 +0200

    feat/130 added eslint, eslint-plugin-testing-library (might need config), and tests for home and memory

--- SquireL/__tests__/HomeScreen-test.tsx
+++ SquireL/__tests__/HomeScreen-test.tsx
@@ -1,11 +1,55 @@
-import { render } from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import  HomeScreen from '@/src/screens/HomeScreen/HomeScreen';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
 
-import HomeScreen, { CustomText } from '@/app/index';
+jest.mock('tamagui', () => {
+  const React = require('react');
+  const { View, Text, TouchableOpacity } = require('react-native');
+  return {
+    Text,
+    View,
+    Image: View,
+    XStack: View,
+    YStack: View,
+    Button: ({ children, ...props }: any) => (
+      <TouchableOpacity accessibilityLabel={props['aria-label']} {...props}>
+        {children}
+      </TouchableOpacity>
+    ),
+  };
+});
+
+type HomeScreenNavigationProp = StackNavigationProp<HomeStackParamList, 'Home'>;
+
+const mockNavigate = jest.fn();
+const mockParentNavigate = jest.fn();
+
+const mockNavigation = {
+  navigate: mockNavigate,
+  getParent: () => ({
+    navigate: mockParentNavigate,
+  }),
+} as unknown as HomeScreenNavigationProp;
 
 describe('<HomeScreen />', () => {
-  test('Text renders correctly on HomeScreen', () => {
-    const { getByText } = render(<HomeScreen />);
-
-    getByText('Welcome!');
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
+
+
+  test('button on Homescreen redirect to MemoryScreen', () => {
+    render(<HomeScreen navigation={mockNavigation as HomeScreenNavigationProp} />);
+    const button = screen.getByLabelText('goToMemoryButton');
+    fireEvent.press(button);
+    expect(mockNavigate).toHaveBeenCalledWith('Memory');
+  });
+
+  test('renders background image', () => {
+    render(<HomeScreen navigation={mockNavigation as HomeScreenNavigationProp} />);
+    const backgroundImage = screen.getByTestId('background-image-home');
+    expect(backgroundImage).toBeTruthy();
+  })
+
 });
