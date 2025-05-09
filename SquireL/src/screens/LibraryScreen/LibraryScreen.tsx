b3b8bb84992b42f3c4541e0b884f86f800a78165
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-05-09 20:57:04 +0200

    feat/135 added fonts, added form in modal

--- SquireL/src/screens/LibraryScreen/LibraryScreen.tsx
+++ SquireL/src/screens/LibraryScreen/LibraryScreen.tsx
@@ -111,7 +111,7 @@ type node = {
         <Text
           fontSize={40}
           color={"#fff"}
-          fontFamily="MedievalSharp-Regular"
+          fontFamily="MysteryQuest_400Regular"
           style={styles.pageTitle}>
               AudioBooks Library
         </Text>
@@ -127,7 +127,7 @@ type node = {
         <Tabs.List>
         { podcasts.map((podcast)=> (
           <Tabs.Tab key={podcast.id} value={podcast.id} onPress={() => changeSelectedPodcast(podcast)}>
-             <Text>{podcast.name}</Text>
+             <Text fontFamily="BubblegumSans_400Regular">{podcast.name}</Text>
           </Tabs.Tab>
         ))}
         </Tabs.List>
@@ -138,12 +138,14 @@ type node = {
           <Tabs.Content style={styles.scrollerView} value={podcast.id}>
           <YStack style={styles.podcastTitlesContainer}>
             <Text
+            fontFamily="BubblegumSans_400Regular"
             alignSelf='center'
             fontSize={40}
             color={"#fff"}>
                 {data?.showByUrl.title}
             </Text>
             <Text
+            fontFamily="BubblegumSans_400Regular"
             alignSelf='center'
             fontSize={30}
             color={"#fff"}>
@@ -162,7 +164,8 @@ type node = {
                         <Image style={styles.episodeImage} source={podcast.image}>
 
                         </Image>
-                        <Text fontSize={15} style={styles.episodeTitle}>
+                        <Text
+                        style={styles.episodeTitle}>
                           {node.title}
                         </Text>
                       </XStack>
@@ -181,20 +184,20 @@ type node = {
           setModalVisible= {setModalVisible}
           modalVisible= {modalVisible} >
             {    
-                  Platform.OS === 'web' ? (
-                  <iframe
-                  style={styles.playerWeb}
+              Platform.OS === 'web' ? (
+              <iframe
+              style={styles.playerWeb}
+              key={episodeInfos.id}
+              frameBorder="0"
+              title={episodeInfos.podcastEpisode.title}
+              src={episodeInfos.podcastEpisode.playerUrl}
+              />
+              ) : (
+                <WebView
                   key={episodeInfos.id}
-                  frameBorder="0"
-                  title={episodeInfos.podcastEpisode.title}
-                  src={episodeInfos.podcastEpisode.playerUrl}
-                  />
-                ) : (
-                  <WebView
-                    key={episodeInfos.id}
-                    source={{ uri: episodeInfos.podcastEpisode.playerUrl }}
-                  />
-                )
+                  source={{ uri: episodeInfos.podcastEpisode.playerUrl }}
+                />
+              )
             }
             <Button size="$2" style={styles.modalCloseButton} onPress={() => setModalVisible(false)}>
               <FontAwesomeIcon icon={faXmark} style={{color: '#fff'}} />
@@ -262,8 +265,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   episodeTitle: {
+    fontFamily: "BubblegumSans_400Regular",
+    fontSize: 20,
     color: '#fff',
-    fontSize: 23,
   },
   episodeImage: {
     width: 160,
