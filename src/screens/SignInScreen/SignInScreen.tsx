9110a71aaf0d7a7e3fdfe777c75095681a653de8
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-06-18 10:38:58 +0200

    fix on signin

--- src/screens/SignInScreen/SignInScreen.tsx
+++ src/screens/SignInScreen/SignInScreen.tsx
@@ -80,7 +80,7 @@ export default function SignInScreen({ navigation }: Readonly<Props>) {
       console.log('axios called');
       await axios({
         method: 'post',
-        url: `${API_URL}/auth/signIn`,
+        url: `${API_URL}/auth/signin`,
         data: { username, password },
       })
         .then(async function (response) {
