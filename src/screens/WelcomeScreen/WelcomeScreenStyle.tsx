a17ccacb45f0342bc8290bfd7b4a9d56f8647fad
Elsa Lécureux <elsa.lecureux@gmail.com>
2025-06-28 19:11:30 +0200

    feat/50 added images for  divers icons, improved responsive desing on welcome screen

--- src/screens/WelcomeScreen/WelcomeScreenStyle.tsx
+++ src/screens/WelcomeScreen/WelcomeScreenStyle.tsx
@@ -6,10 +6,9 @@ export const styles = StyleSheet.create({
     backgroundColor: '#000000',
     width: '100%',
     height: '100%',
+    justifyContent: 'center',
   },
   titleWelcomePage: {
-    width: '70%',
-    height: '16%',
     alignSelf: 'center',
   },
 });
